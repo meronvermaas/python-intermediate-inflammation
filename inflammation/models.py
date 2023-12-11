e3862676791d6ddb6a96f93508f9a134f119487f
meronvermaas <37903632+meronvermaas@users.noreply.github.com>
2023-12-11 11:55:02 +0100

    Code style fixes

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,7 +15,7 @@ def load_csv(filename):
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=',')
+    return np.loadtxt(fname=filename, delimiter=",")
 
 
 def daily_mean(data):
@@ -31,4 +31,3 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
-
